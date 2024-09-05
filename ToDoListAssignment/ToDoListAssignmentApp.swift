b79086964fd4c00e24bef988008a3aa3376cae6c
Rahul Narayanan <rahulon@icloud.com>
2024-09-05 00:28:19 -0400

    Initial Commit

---
+++ ToDoListAssignment/ToDoListAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAssignmentApp.swift
+//  ToDoListAssignment
+//
+//  Created by Rahul on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
