3e5999a7ecb8937d2c0a3fca9afa4046a3967977
Cheemalamarri, Srisankaet <scheemal3@gatech.edu>
2024-09-05 11:19:59 -0400

    Making empty project

---
+++ VineyardAssignment/VineyardAssignment/VineyardAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  VineyardAssignmentApp.swift
+//  VineyardAssignment
+//
+//  Created by Sankaet Cheemalamarri on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VineyardAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
